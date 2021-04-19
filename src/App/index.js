cf0f8367cecd1da63503652d8052dc100a4ba860
Greenfin17 <jmaple@yahoo.com>
2021-04-18 21:19:00 -0500

    react basic setup

---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>INSIDE APP COMPONENT</h2>
+      <button className='btn btn-info'>I am a button</button>
+      <span className ="fas fa-camera"></span>
+
+    </div>
+  );
+}
+
+export default App;
